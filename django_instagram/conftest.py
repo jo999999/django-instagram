848cda01a95fd4388461047a7b4057764946a90f
haneul jo <chnheaven@gmail.com>
2021-09-02 14:24:03 +0900

    first commit

---
+++ django_instagram/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_instagram.users.models import User
+from django_instagram.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
